a82a5bbdbd168d8c70e3e164be44e624ece8686a
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-20 15:08:54 +0000

    welcome message, test not passing

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,8 @@
+
+
+feature 'displays a welcome message', type: :feature do
+  scenario 'tests infrastructure' do
+    visit '/'
+      expect(page).to have_content "Testing infrastructure working!"
+  end
+end
