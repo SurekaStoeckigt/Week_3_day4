92daad635ce4dcb7b9e636a9d0150e3e0361a9c4
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-26 09:43:02 +0000

    displaying hitpoints for player2 after attack

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,10 +6,10 @@ feature 'players attack and receive confirmation' do
     expect(page).to have_content "Jimmy has attacked Hoju!"
   end
 
-  # scenario 'reduces Player2s hit points when Player1 attacks Player2'  do
-  #   sign_in_and_play
-  #   click_button "Attack Player 2!"
-  #   expect(page).to have_content "Hoju: 50 HP"
-  # end
+  scenario 'reduces Player2s hit points when Player1 attacks Player2'  do
+    sign_in_and_play
+    click_button "Attack Player 2!"
+    expect(page).to have_content "Jimmy has attacked Hoju! Hoju: 50HP"
+  end
 
 end
